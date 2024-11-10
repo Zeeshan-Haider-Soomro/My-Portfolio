9b1221fd4846800950d1f579ffd8dbbd1fc03236
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-11-10 20:18:08 +0500

    done

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import Work from "@/components/Work";
+import Cta from "@/components/Cta";
+import Services from "@/components/Services";
+
+export default function Home() {
+  return (
+ <main>
+  <Hero/>
+  <About/>
+  <Services/>
+  <Work/>
+  <Cta/>
+ </main>
+  );
+}
---
+++ components/Logo.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+// import Image from "next/image";
+
+const Logo = () => {
+  return (
+    <div>
+    
+    <Link href='/'>
+    {/* <Image size='100px' src='/zeeshanLogo.png' width={54} height={54} priority alt=""/> */}
+    <h1 className="text-3xl m-3 ">Portfolio</h1>
+    
+    </Link>
+    </div>
+  )
+}
+
+export default Logo
