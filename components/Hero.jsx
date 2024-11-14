0873ba6eb36131513a5c50a5c7f4725cd5df36a2
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-11-15 00:59:56 +0500

    done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -91,7 +91,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce2">
+        <div className="md:flex absolute left-2/4 bottom-8 xl:bottom-12 animate-bounce2 ">
           <RiArrowDownSFill className="text-3xl text-primary animate-bounce" />
         </div>
       </div>
